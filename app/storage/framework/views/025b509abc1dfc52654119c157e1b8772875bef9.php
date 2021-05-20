bf680ebf0b16c27e4ec5ae412d747e94b4ec88b0
alifalghiffari <82097248+alifalghiffari@users.noreply.github.com>
2021-05-20 14:23:06 +0700

    Commit Sprint 1

---
+++ app/storage/framework/views/025b509abc1dfc52654119c157e1b8772875bef9.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<!--navigasi-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container">
+    <a class="navbar-brand">
+        <img src="images/Green and Red Diagonal Lines Sports Logo.png" width="74" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo e(url('/')); ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo e(url('/aktivitas')); ?>">Aktivitas</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo e(url('/pemberitahuan')); ?>">Pemberitahuan</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <div class="dropdown">
+  <button class="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  <a class="btn" href="#"><img src="images/user.png" width="35" alt=""></a>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="<?php echo e(url('/login')); ?>">Profil</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Log Out</a></li>
+  </ul>
+</div>
+            </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+
+<!--Halaman-->
+<section class="tournament mt-5">
+    <div class="container">
+        <div class="row">
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Teman
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Shobri
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Irfan
+                    </label>
+                  </div>
+                  </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Jadwal
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Jumat, 21 mei 2021
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Sabtu, 22 mei 2021
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Minggu, 23 mei 2021
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Lokasi
+                </button>
+              </h2>
+              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Lapangan Pango
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Lapangan Punge
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a class="btn btn-primary-c mt-3" href="<?php echo e(url('/aktivitas')); ?>" role="button">Selesai</a>
+          </div>
+    </div>
+</div>
+</section>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html><?php /**PATH C:\Users\MSI Modern\Downloads\appRaravel\appRaravel\app_laravel\resources\views/permainan.blade.php ENDPATH**/ ?>
---
+++ app/storage/framework/views/691408f373e7551a35147f2a61bfcf3d72ad646d.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<!--navigasi-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container">
+    <a class="navbar-brand">
+        <img src="images/Green and Red Diagonal Lines Sports Logo.png" width="74" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo e(url('/')); ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo e(url('/aktivitas')); ?>">Aktivitas</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo e(url('/pemberitahuan')); ?>">Pemberitahuan</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="btn btn-login" href="#" role="button">Login</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+
+<!--Halaman-->
+<section class="tournament mt-5">
+    <div class="container">
+        <div class="row">
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Pemberitahuan
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>Pemberitahuan 1</strong>Halaman
+                </div>
+                <section class="buttonnotf mb-2">
+                  <div class="container">
+                <a class="btn btn-secondary-b" href="menu.html" role="button">Tolak</a>
+                <a class="btn btn-primary-b" href="menu.html" role="button">Terima</a>
+              </section>
+              </div>
+              </div>
+            </div>
+            <section class="buttontour mt-3">
+            <a class="btn btn-secondary-a" href="<?php echo e(url('/menu')); ?>" role="button">Kembali</a>
+          </section>
+    </div>
+</div>
+</section>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html><?php /**PATH D:\RPL-RD6\app_laravel\resources\views/Pemberitahuan.blade.php ENDPATH**/ ?>
