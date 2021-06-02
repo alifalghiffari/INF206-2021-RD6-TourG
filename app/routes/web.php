6e89356527d547db8590608a6c7d3bc57b60f922
alifalghiffari <82097248+alifalghiffari@users.noreply.github.com>
2021-06-02 22:53:23 +0700

    memperbarui function middleware

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,22 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
-});
-
-Route::get('/utama', function () {
-    return view('home');
 });
 
 Route::get('/profil', function () {
     return view('profil');
 });
 
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth', 'ceklevel:User']], function() {
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/aktivitas', [App\Http\Controllers\AktivitasController::class, 'index'])->name('aktivitas');
 Route::get('/pemberitahuan', [App\Http\Controllers\PemberitahuanController::class, 'index'])->name('pemberitahuan');
 Route::get('/menu', [App\Http\Controllers\PermainanController::class, 'index'])->name('menu');
-Route::get('/menu/permainan', [App\Http\Controllers\PermainanController::class, 'utama'])->name('utama');
+Route::get('/permainan', [App\Http\Controllers\PermainanController::class, 'permainan'])->name('permainan');
+
+});
+
+Route::group(['middleware' => ['auth', 'ceklevel:Admin']], function() {
+
+Route::get('/home', [App\Http\Controllers\AdminController::class, 'index'])->name('indexadmin');
+Route::get('/aktadmin', [App\Http\Controllers\AdminController::class, 'aktivitas'])->name('aktivitasadmin');
+Route::get('/pembadmin', [App\Http\Controllers\AdminController::class, 'pemberitahuan'])->name('pemberitahuanadmin');
+
+});
